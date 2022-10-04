7b4d0564501bc216bef0c1cf66a1c969c744b7d8
Sky <jafarsky@users.noreply.github.com>
2022-10-04 17:23:25 +0300

    add axios in main js

--- main.js
+++ main.js
@@ -28,8 +28,7 @@ var dataProduct = [];
 let copyEditIndex = 0;   // عن طريق اخذ نسخة منه his block خارج local حتى نحل مشكلة عدم استخدام المتغير global هذا متغير
 var totalResult = 0;
 
-const axios = require('axios');
-
+const { default: axios } = require('axios');
 
 
 if (localStorage.product != undefined) {
